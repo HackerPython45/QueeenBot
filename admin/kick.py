33c8a7d0706ef36d935f96eae7b1d0ef5fb6a566
HackerPython45 <155677324+HackerPython45@users.noreply.github.com>
2025-05-09 19:14:52 +0300

    system kick

--- admin/kick.py
+++ admin/kick.py
@@ -12,15 +12,16 @@ class Kick(commands.Cog):
     async def kick(self, inter, участник: disnake.Member = commands.Param(description='Участник'), причина: str = commands.Param(description='Причина')):
         if участник.bot: return await inter.response.send_message(f'Вы не можете кикнуть бота', ephemeral=True)
         if участник.id == inter.author.id: return await inter.response.send_message('Вы не можете заблокировать самого себя', ephemeral=True)
-        if inter.author.has_permissions.administrator:
-            embed = disnake.Embed(title='KICK')
-            embed.add_field(name='Администратор: ', value=f'> {inter.author.mention}', inline=False)
-            embed.add_field(name='Кикнутый: ', value=f'> {участник.mention}')
-            embed.add_field(name='Причина: ', value=f'> `{причина}`')
-            await участник.kick(reason=причина)
-            await inter.response.send_message(embed=embed)
-        else:
-            await inter.response.send_message('Вы не являетесь администратором', ephemeral=True)
+        
+        if inter.author.top_role.position <= участник.top_role.position: return await inter.response.send_message('❌ Вы не можете кикнуть участника с равной или более высокой ролью', ephemeral=True)
+        if участник.id == inter.author.id: return await inter.response.send_message('❌ Вы не можете кикнуть самого себя', ephemeral=True)
+
+        embed = disnake.Embed(title='KICK')
+        embed.add_field(name='Администратор: ', value=f'> {inter.author.mention}', inline=False)
+        embed.add_field(name='Кикнутый: ', value=f'> {участник.mention}')
+        embed.add_field(name='Причина: ', value=f'> `{причина}`')
+        await участник.kick(reason=причина)
+        await inter.response.send_message(embed=embed)
 
 def setup(bot):
     bot.add_cog(Kick(bot))
