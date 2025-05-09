5280f08032962e2ac1113a5476a0004873baf5fb
HackerPython45 <155677324+HackerPython45@users.noreply.github.com>
2025-05-09 19:11:34 +0300

    Проверить

--- admin/kick.py
+++ admin/kick.py
@@ -12,7 +12,7 @@ class Kick(commands.Cog):
     async def kick(self, inter, участник: disnake.Member = commands.Param(description='Участник'), причина: str = commands.Param(description='Причина')):
         if участник.bot: return await inter.response.send_message(f'Вы не можете кикнуть бота', ephemeral=True)
         if участник.id == inter.author.id: return await inter.response.send_message('Вы не можете заблокировать самого себя', ephemeral=True)
-        if inter.author.guild_permissions.administrator:
+        if inter.author.has_permissions.administrator:
             embed = disnake.Embed(title='KICK')
             embed.add_field(name='Администратор: ', value=f'> {inter.author.mention}', inline=False)
             embed.add_field(name='Кикнутый: ', value=f'> {участник.mention}')
