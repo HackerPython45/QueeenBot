cd3595a7e9f9cbd93f85a449808406168d41d266
HackerPython45 <155677324+HackerPython45@users.noreply.github.com>
2025-05-10 17:11:39 +0300

    2

--- admin/set_reputation.py
+++ admin/set_reputation.py
@@ -15,7 +15,7 @@ class SetReputation(commands.Cog):
     @commands.slash_command(name='set-reputations', description='Выдать репутацию')
     async def set_rep(self, inter, участник: disnake.Member = commands.Param(description='Выберите участника'), сколько: str = commands.Param(description='Сколько выдать')):
         guild_info = self.db.find_one({"guil_id": inter.guild.id})
-        if сколько <= 0: return await inter.response.send_message('Вы не можете выдать 0 или меньше 0 репутации', ephemeral=True)
+        if int(сколько) <= 0: return await inter.response.send_message('Вы не можете выдать 0 или меньше 0 репутации', ephemeral=True)
 
         self.db.set_give_reputation_user(inter.guid.id, участник.id, сколько)
         await inter.response.send_message(f'**Репутация** ({сколько}) было устоновлено {участник.mention}', ephemeral=True)
@@ -27,7 +27,7 @@ class SetReputation(commands.Cog):
         user_info = guild_info['economy']['users']
         if сколько != участник(user_info.get('reputation')):
             return await inter.response.send_message('Вы не можете забрать репутацию у пользователя если равно 0', ephemeral=True)
-        if сколько <= 0: return await inter.response.send_message('Вы не можете забрать 0 или меньше 0 репутации', ephemeral=True)
+        if int(сколько) <= 0: return await inter.response.send_message('Вы не можете забрать 0 или меньше 0 репутации', ephemeral=True)
 
         self.db.set_give_reputation_user(inter.guid.id, участник.id, сколько)
         await inter.response.send_message(f'**Репутация** ({сколько}) было забрано {участник.mention}', ephemeral=True)
